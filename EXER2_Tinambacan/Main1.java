e4867d39320b4cf1524c4be429532c60c0d6ee41
Arlem Ace Tinambacan <arlemacetinambacan@gmail.com>
2025-09-08 22:48:13 +0800

    Add files via upload

---
+++ EXER2_Tinambacan/Main1.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Drone drone1 = new Drone();
+        drone1.displayInfo();
+
+        System.out.println("--------------------");
+
+        Drone drone2 = new Drone("White", "DJI-Mini3", "DRN-123456");
+        drone2.displayInfo();
+
+        System.out.println("--------------------");
+
+        Drone drone3 = new Drone("DRN-999999");
+        drone3.displayInfo();
+    }
+}
